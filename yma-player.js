e895a35ab0eec8f4733f7734b88fdc926eb20eac
Yannick Mauray <yannick.mauray@gmail.com>
2016-06-28 15:44:30 +0200

    More updates

--- yma-player.js
+++ yma-player.js
@@ -3,7 +3,86 @@
     'use strict';
 
     $(document).ready(function() {
-        console.log('Document ready !');
+
+        var audios = $('.yma_player audio');
+        audios.on('play', function(event) {
+            var target = event.target;
+            var id = $(target).attr('data-player-id');
+            var player = $('#yma_player_' + id);
+        });
+
+        audios.on('pause', function(a, b, c, d, e, f) {
+            
+        });
+
+        audios.on('timeupdate', function(event) {
+            var target = event.target;
+            var id = $(target).attr('data-player-id');
+            var player = $('#yma_player_' + id);
+            if (target.duration > 0) {
+                var advance = 100 * target.currentTime / target.duration;
+                $('.yma_player_progress_bar', player).css('width', advance + '%');
+            }
+            var options = $('datalist option', player);
+            for (var i = options.length - 1; i >= 0; i--) {
+                var option = options[i];
+                var index = $(option).attr('data-index');
+                if (index < target.currentTime) {
+                    var title = $(option).val();
+                    var performer = $(option).attr('data-performer');
+                    $('.yma_player_artist', player).text(performer).removeClass('single');
+                    $('.yma_player_title', player).text(title);
+                    break;
+                }
+            }
+        });
+
+        $('.yma_player_background_bar').click(function(event) {
+            var player_id = $(this).attr('data-player-id');
+            var audio = $('audio#yma_player_audio_' + player_id)[0];
+            var width = event.target.offsetWidth;
+            var position = event.offsetX;
+            var percent = position / width;
+            audio.currentTime = audio.duration * percent;
+        });
+
+        $('.yma_player_progress_bar').click(function(event) {
+            var player_id = $(this).attr('data-player-id');
+            var audio = $('audio#yma_player_audio_' + player_id)[0];
+            var width = event.target.offsetWidth;
+            var position = event.offsetX;
+            var percent = position / width;
+            audio.currentTime = audio.currentTime * percent;
+        });
+
+        $('.yma_player_play_button').click(function(event) {
+            event.preventDefault();
+            var play_circle = $('.fa-play-circle', $(this));
+            var player_id = $(this).attr('data-player-id');
+            var audio = $('audio#yma_player_audio_' + player_id)[0];
+            $('.fa', $(this)).toggleClass('fa-play-circle');
+            $('.fa', $(this)).toggleClass('fa-pause-circle');
+            if (play_circle.length == 1) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        });
+
+        var volume_bars = $('.yma_player_volume_bar');
+        volume_bars.each(function(index, item) {
+            var bar_height = this.offsetHeight;
+            var button = $('.yma_player_volume_button', this);
+            var button_height = button[0].offsetHeight;
+            var top = (bar_height * .75) - (button_height / 2);
+            button.css('bottom', top + 'px');
+            button.show();
+        });
+
+        volume_bars.click(function(event) {
+            console.log(event.offsetY);
+            console.log(event.target.offsetHeight);
+        });
     });
     
 }(window.jQuery));
--- yma-player.php
+++ yma-player.php
@@ -39,18 +39,19 @@ add_filter( 'powerpress_player', function ( $content, $media_url, $EpisodeData =
 	switch ( $extension ) {
 		case 'mp3':
 		case 'ogg':
-			$content .= build_player_player( $media_url, $EpisodeData );
+			$content .= build_player_player( $media_url, $cue_sheet, $EpisodeData );
 			break;
 		default:
 			add_filter( 'powerpress_player', 'powerpressplayer_player_audio', 10, 3 );
 	}
 
 	return $content;
-}, 3, 3 );
+}, 2, 3 );
 
 add_action( 'init', function () {
 	wp_enqueue_script( 'yma-player', plugin_dir_url( PLAYER_MAIN_FILE ) . 'yma-player.js', array( 'jquery' ) );
 	wp_enqueue_style( 'yma-player', plugin_dir_url( PLAYER_MAIN_FILE ) . 'yma-player.css' );
+	wp_enqueue_style( 'fontawesome', plugin_dir_url( PLAYER_MAIN_FILE ) . 'font-awesome-4.6.3/css/font-awesome.min.css' );
 } );
 
 /**
@@ -59,12 +60,51 @@ add_action( 'init', function () {
  *
  * @return string
  */
-function build_player_player( $media_url, $EpisodeData = array() ) {
+function build_player_player( $media_url, $cue_sheet, $EpisodeData = array() ) {
+	global $post;
 	$player_id = powerpressplayer_get_next_id();
+	$cue_lines = preg_split("/\n/", $cue_sheet . '\r\n');
+	$intag = FALSE;
+	$options = '';
+	$title = '';
+	$performer = '';
+	$index = '';
+	$timestamp = 0;
+	foreach ($cue_lines as $cue_line) {
+		$clean_line = trim( $cue_line );
+		if (substr($clean_line, 0, 6) == 'TRACK ') {
+			if ( $intag ) {
+				$options .= "<option value='{$title}' data-performer='{$performer}' data-index='{$timestamp}'>";
+			}
+			$intag = TRUE;
+		} else if (substr( $clean_line, 0, 6 ) == 'TITLE ') {
+			$title = substr( $clean_line, 7, -1 );
+		} else if (substr($clean_line, 0, 10) == 'PERFORMER ') {
+			$performer = substr( $clean_line, 11, - 1 );
+		} else if (substr($clean_line, 0, 9) == 'INDEX 01 ') {
+			$index = substr( $clean_line, 9 );
+			$numbers = explode(':', $index);
+			$timestamp = ((60 * 100 * $numbers[0]) + (100 * $numbers[1]) + $numbers[2]) / 100;
+		}
+	}
+	$options .= "<option value='{$title}' data-performer='{$performer}' data-index='{$timestamp}'>";
 	$content   = <<<EOF
 <div class="powerpress_player" id="powerpress_player_{$player_id}">
-	<div class="yma_player">
-		<div class="yma_player_play_button"></div>
+	<div class="yma_player" id="yma_player_{$player_id}">
+		<div class="yma_player_display">
+			<div class="yma_player_post_title">{$post->post_title}</div>
+			<div class="yma_player_track_info single"><span class="yma_player_artist single">—</span><span class="yma_player_title"></span></div>
+		</div>
+		<div class="yma_player_play_button" data-player-id="{$player_id}"><i class="fa fa-play-circle"></i></div>
+		<div class="yma_player_background_bar" data-player-id="{$player_id}"></div>
+		<div class="yma_player_progress_bar" data-player-id="{$player_id}"></div>
+		<div class="yma_player_volume_bar data-player-id="{$player_id}">
+			<div class="yma_player_volume_button data-player-id="{$player_id}"></div>
+		</div>
+		<audio id="yma_player_audio_{$player_id}" data-player-id="{$player_id}">
+			<source src="{$media_url}" type="{$EpisodeData['type']}">
+		</audio>
+		<datalist>{$options}</datalist>
 	</div>
 </div>
 EOF;
